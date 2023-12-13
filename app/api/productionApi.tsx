fe944c118b3fee7d08a4a53618a50b0a44fcce9b
quangtrung <20520333@ms.uit.edu.vn>
2023-12-13 15:29:09 +0700

    add production types and CRU api

---
+++ app/api/productionApi.tsx
@@ -0,0 +1,39 @@
+import apiClient from '../utils/client-api';
+import { IProductionResponse, IProductionsResponse } from '../types/production';
+
+export const getAllProductionOrders = async () => {
+  const response =
+    await apiClient.get<IProductionsResponse>(`ProductionOrders`);
+  return response.data;
+};
+
+export const getProductionOrder = async (id: string) => {
+  const response = await apiClient.get<IProductionResponse>(
+    `PurchaseOrders/${id}`,
+  );
+  return response.data;
+};
+
+export const createProductionOrder = async (
+  production: IProductionResponse,
+) => {
+  const response = await apiClient.post<IProductionResponse>(
+    `PurchaseOrders`,
+    production,
+  );
+  return response.data;
+};
+
+export const updatePurchaseOrder = async ({
+  id,
+  production,
+}: {
+  id: string;
+  production: IProductionResponse;
+}) => {
+  const response = await apiClient.patch<IProductionResponse>(
+    `PurchaseOrders/${id}`,
+    production,
+  );
+  return response.data;
+};
---
+++ app/types/production.tsx
@@ -0,0 +1,52 @@
+import { Supply } from './requisition';
+import { Event, IUser, Product, productionFacility } from './sales';
+
+export interface supplyUsageItem {
+  quantity: number;
+  supply: Supply;
+  supplyId: string;
+  totalCost: number;
+  unit: string;
+  unitCost: number;
+}
+
+export interface ProductionItem {
+  product: Product;
+  totalCost: number;
+  totalValue: number;
+  unitCost: number;
+  unitValue: number;
+  itemId: string;
+  quantity: number;
+  unit: string;
+}
+
+export interface IProductionResponse {
+  approvalStatus: string;
+  approveProductionManager: IUser;
+  approveProductionManagerId: string;
+  productionFacility: productionFacility;
+  productionFacilityId: string;
+  supplyUsageItems: supplyUsageItem[];
+  totalCost: number;
+  totalProfit: number;
+  totalValue: number;
+  events: Event[];
+  items: ProductionItem[];
+  status: string;
+  createTime: Date;
+  createUser: IUser;
+  createUserId: string;
+  endTime: Date;
+  endUser: IUser;
+  endUserId: string;
+  problem: string;
+  updateTime: Date;
+}
+
+export interface IProductionsResponse {
+  status: string;
+  data: {
+    sales: IProductionResponse[];
+  };
+}
