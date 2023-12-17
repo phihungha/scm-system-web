c31ec00a6f5f25ebb9ded167581d89338b3707e6
quangtrung <20520333@ms.uit.edu.vn>
2023-12-17 21:57:33 +0700

    add time conversion

---
+++ app/utils/time-conversion.tsx
@@ -0,0 +1,9 @@
+export function dateToFullFormat(date: Date) {
+    return date.toLocaleString([], {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
+    });
+  }
