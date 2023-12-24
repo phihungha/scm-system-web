1ad1171b359b76c55ba19980e0db1bd4692e010b
Ha Phi Hung <haphihung55@gmail.com>
2023-12-24 23:45:56 +0700

    Merge sales item files

--- app/sales/orders/[id]/components/SalesOrderItemsEditor.tsx
+++ app/sales/orders/[id]/components/SalesOrderItemsEditor.tsx
@@ -2,11 +2,13 @@
 
 import { getProducts } from '@/app/api/product';
 import AutoCompleteItemPreview from '@/app/components/AutoCompleteItemPreview';
+import ItemEditCard from '@/app/components/ItemEditCard';
 import ItemsEditor from '@/app/components/ItemsEditor';
 import { SalesOrderItem } from '@/app/models/sales-order';
+import CurrencyFormat from '@/app/utils/currency-formats';
+import { Text } from '@chakra-ui/react';
 import { AutoCompleteItem } from '@choc-ui/chakra-autocomplete';
 import { useQuery } from 'react-query';
-import SalesOrderItemEditCard from './SalesOrderItemEditCard';
 
 export interface SalesOrderItemsEditorProps {
   items: SalesOrderItem[];
@@ -77,5 +79,38 @@ export default function SalesOrderItemsEditor({
         ))
       }
     </ItemsEditor>
+  );
+}
+
+interface SalesOrderItemEditCardProps {
+  item: SalesOrderItem;
+  onDelete: (item: SalesOrderItem) => void;
+  onQuantityChange: (item: SalesOrderItem) => void;
+}
+
+function SalesOrderItemEditCard(props: SalesOrderItemEditCardProps) {
+  const item = props.item;
+  const product = props.item.product;
+
+  const onQuantityChange = (quantity: number) =>
+    props.onQuantityChange({
+      ...item,
+      quantity,
+      totalPrice: quantity * product.price,
+    });
+
+  return (
+    <ItemEditCard
+      id={product.id}
+      quantity={item.quantity}
+      onQuantityChange={(_, quantity) => onQuantityChange(quantity)}
+      name={product.name}
+      unit={item.unit}
+      imageUrl={product.imageUrl}
+      onDelete={() => props.onDelete(item)}
+    >
+      <Text>Price: {CurrencyFormat.format(item.unitPrice)}</Text>
+      <Text>Total price: {CurrencyFormat.format(item.totalPrice)}</Text>
+    </ItemEditCard>
   );
 }
