8c565953367d89d1b7d1cb270d572a14d2250700
Ha Phi Hung <haphihung55@gmail.com>
2023-12-25 20:02:02 +0700

    Add utility components for action buttons

--- app/components/buttons.tsx
+++ app/components/buttons.tsx
@@ -1,5 +1,31 @@
-import { Button, ButtonProps } from '@chakra-ui/react';
+import {
+  Button,
+  ButtonProps,
+  HStack,
+  Stack,
+  StackProps,
+  Text,
+} from '@chakra-ui/react';
 
 export function ActionButton(props: ButtonProps) {
   return <Button width={100} variant="solid" size="lg" {...props} />;
+}
+
+export interface ActionButtonRowProps extends ButtonProps {
+  buttonText: string;
+}
+
+export function ActionButtonRow(props: ActionButtonRowProps) {
+  return (
+    <HStack spacing={6} alignItems="center">
+      <Button width={200} variant="solid" {...props}>
+        {props.buttonText}
+      </Button>
+      <Text>{props.children}</Text>
+    </HStack>
+  );
+}
+
+export function ActionButtonSection(props: StackProps) {
+  return <Stack spacing={3} {...props} />;
 }
