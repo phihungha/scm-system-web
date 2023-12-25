b2f3e3925fea0909713efac91e6eec3d657ed730
Ha Phi Hung <haphihung55@gmail.com>
2023-12-26 01:54:27 +0700

    Implement supply search screen

--- app/components/SideBar.tsx
+++ app/components/SideBar.tsx
@@ -16,6 +16,8 @@ import {
   FiBox,
   FiCpu,
   FiDownload,
+  FiDroplet,
+  FiGrid,
   FiHome,
   FiSettings,
   FiUpload,
@@ -61,8 +63,10 @@ const navbarItems = [
   { href: '/purchases/orders', name: 'Purchases', icon: FiDownload },
   { href: '/production/orders', name: 'Production', icon: FiCpu },
   { href: '/sales/orders', name: 'Sales', icon: FiUpload },
-  { href: '/inventory/stock', name: 'Inventory', icon: FiBox },
+  { href: '/inventory/stock', name: 'Inventory', icon: FiGrid },
   { href: '/reports/sales', name: 'Reports', icon: FiBarChart },
+  { href: '/products', name: 'Products', icon: FiBox },
+  { href: '/supplies', name: 'Supplies', icon: FiDroplet },
   { href: '/settings', name: 'Settings', icon: FiSettings },
 ];
 
---
+++ app/components/SimpleItemSearchPanel.tsx
@@ -0,0 +1,84 @@
+'use client';
+
+import {
+  Checkbox,
+  HStack,
+  IconButton,
+  Input,
+  InputGroup,
+  InputRightAddon,
+  Select,
+  Stack,
+} from '@chakra-ui/react';
+import { useState } from 'react';
+import { FiSearch } from 'react-icons/fi';
+import {
+  SimpleItemQueryParams,
+  SimpleItemSearchCriteria,
+} from '../models/general';
+
+export interface SimpleItemSearchPanelProps {
+  queryParams: SimpleItemQueryParams;
+  setQueryParams: (params: SimpleItemQueryParams) => void;
+}
+
+export default function SimpleItemSearchPanel({
+  queryParams,
+  setQueryParams,
+}: SimpleItemSearchPanelProps) {
+  const [searchTerm, setSearchTerm] = useState(queryParams.searchTerm);
+  const [searchCriteria, setSearchCriteria] = useState(
+    queryParams.searchCriteria as string,
+  );
+  const [displayAll, setDisplayAll] = useState(false);
+
+  const onSearchClick = () =>
+    setQueryParams({
+      ...queryParams,
+      searchTerm,
+      searchCriteria: searchCriteria as SimpleItemSearchCriteria,
+    });
+
+  return (
+    <Stack spacing={5}>
+      <HStack spacing={3}>
+        <InputGroup>
+          <Input
+            placeholder="Enter something to search..."
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.key === 'Enter') {
+                onSearchClick();
+              }
+            }}
+          />
+          <InputRightAddon>
+            <IconButton
+              mx={5}
+              aria-label="Search"
+              icon={<FiSearch color="gray.300" />}
+              onClick={onSearchClick}
+            />
+          </InputRightAddon>
+        </InputGroup>
+
+        <Select
+          w={330}
+          value={searchCriteria}
+          onChange={(e) => setSearchCriteria(e.target.value)}
+        >
+          <option value="Id">ID</option>
+          <option value="Name">Name</option>
+        </Select>
+      </HStack>
+
+      <Checkbox
+        isChecked={displayAll}
+        onChange={(e) => setDisplayAll(!displayAll)}
+      >
+        Display all
+      </Checkbox>
+    </Stack>
+  );
+}
