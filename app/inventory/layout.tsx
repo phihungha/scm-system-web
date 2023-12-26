ff51c48416f9c28e690a31c877ebc94ca1b8efd0
Ha Phi Hung <haphihung55@gmail.com>
2023-12-26 17:27:40 +0700

    Rename

--- app/inventory/layout.tsx
+++ app/inventory/layout.tsx
@@ -2,12 +2,12 @@ import { Stack } from '@chakra-ui/react';
 import { NavTabs } from '../components/nav';
 
 const navTabItems = [
-  { url: '/inventory/productStock', name: 'Product stock' },
-  { url: '/inventory/supplyStock', name: 'Supply stock' },
-  { url: '/inventory/productsIssue', name: 'Products issue' },
-  { url: '/inventory/productsReceive', name: 'Products receive' },
-  { url: '/inventory/suppliesIssue', name: 'Supplies issue' },
-  { url: '/inventory/suppliesReceive', name: 'Supplies receive' },
+  { url: '/inventory/product-stock', name: 'Product stock' },
+  { url: '/inventory/supply-stock', name: 'Supply stock' },
+  { url: '/inventory/products-issue', name: 'Products issue' },
+  { url: '/inventory/products-receive', name: 'Products receive' },
+  { url: '/inventory/supplies-issue', name: 'Supplies issue' },
+  { url: '/inventory/supplies-receive', name: 'Supplies receive' },
 ];
 
 export default function InventoryLayout({
