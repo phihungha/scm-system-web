d0633b772d711d2f0a9dc1bcf84cde666c389069
Ha Phi Hung <haphihung55@gmail.com>
2023-12-29 00:46:59 +0700

    Implement user list screen

--- app/models/user.ts
+++ app/models/user.ts
@@ -3,23 +3,30 @@ import {
   SoftDeletable,
   SoftDeletableParams,
 } from './general';
+import { ProductionFacility } from './production-facility';
 
 type Gender = 'Male' | 'Female' | 'Other';
 
 export interface User extends CreateUpdateTime, SoftDeletable {
   id: string;
   userName: string;
-  email: string;
-  phoneNumber: string;
+  imageUrl?: string;
+  imageName?: string;
+  email?: string;
+  phoneNumber?: string;
   name: string;
   gender: Gender;
   dateOfBirth: Date;
   idCardNumber: string;
   address: string;
+  roles: string[];
+  productionFacilityId?: number;
+  productionFacility?: ProductionFacility;
 }
 
 export interface UserCreateParams extends SoftDeletableParams {
   address: string;
+  imageName?: string;
   dateOfBirth: Date;
   description: string;
   email: string;
@@ -28,6 +35,7 @@ export interface UserCreateParams extends SoftDeletableParams {
   name: string;
   phoneNumber: string;
   roles: string[];
+  productionFacilityId?: number;
   userName: string;
   password: string;
 }
@@ -35,14 +43,20 @@ export interface UserCreateParams extends SoftDeletableParams {
 export interface UserUpdateParams extends SoftDeletableParams {
   id: number;
   address: string;
+  imageName?: string;
   dateOfBirth: Date;
   description: string;
   email: string;
   gender: Gender;
   idCardNumber: string;
   name: string;
-  productionFacilityId: number;
+  productionFacilityId?: number;
   phoneNumber: string;
   roles: string[];
   userName: string;
+}
+
+export interface UserPasswordUpdateParams {
+  oldPassword: string;
+  newPassword: string;
 }
